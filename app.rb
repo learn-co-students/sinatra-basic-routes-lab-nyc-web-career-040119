e76f14580e2d3414bf68c334149262680a70080a
Edgar Ong <emailedgar@gmail.com>
2019-04-21 21:50:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  resp = Rack::Response.new
+
+  get '/name' do
+    resp.status = 200
+    resp.write "My name is "
+  end
+
+  get '/hometown' do
+    resp.status = 200
+    resp.write "My hometown is "
+  end
+
+  get '/favorite-song' do
+    resp.status = 200
+    resp.write "My favorite song is "
+  end
+
+  resp.finish
 end
