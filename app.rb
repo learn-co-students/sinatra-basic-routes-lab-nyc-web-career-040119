ac702ee439a1dfcfc123f0b05ca74e7009ae9cb2
danfyfe <fyfedaniel@gmail.com>
2019-04-21 20:21:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Dan"
+  end
+
+  get "/hometown" do
+    "My hometown is Montvale"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is the EDF theme"
+  end
+
 end
