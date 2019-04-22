edf995a699a0abf46a474658171374d59704899c
Edgar Ong <emailedgar@gmail.com>
2019-04-21 21:52:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,17 +5,17 @@ class App < Sinatra::Base
 
   get '/name' do
     resp.status = 200
-    resp.write "My name is "
+    resp.write "My name is Edgar"
   end
 
   get '/hometown' do
     resp.status = 200
-    resp.write "My hometown is "
+    resp.write "My hometown is NYC"
   end
 
   get '/favorite-song' do
     resp.status = 200
-    resp.write "My favorite song is "
+    resp.write "My favorite song is What's Up Danger by Blackway, at the moment"
   end
 
   resp.finish
