d431b3d48228cdbf6c5195447bb3861ba450a5e2
William Sinn <w.h.sinn@gmail.com>
2019-04-22 14:11:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+      "hello world"
+    end 
+    get '/name' do
+      "My name is William Sinn"
+    end
+    get '/hometown' do
+      "My hometown is New York"
+    end
+    get '/favorite-song' do
+      "My favorite song is Sing Song"
+    end
 end
